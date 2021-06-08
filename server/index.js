655eac00dba4f84e1a16f1fed45c8a07f7c4f145
RyFogle <13fogle@gmail.com>
2021-06-08 16:37:36 -0700

    adding mongoServer set up

--- server/index.js
+++ server/index.js
@@ -1,11 +1,28 @@
 import express from 'express'; 
 import mongoose from 'mongoose'; 
 import cors from 'cors';
+import dotenv from 'dotenv'; 
 
-const app = express(); 
+dotenv.config(); 
+
+const app = express();
+const port = process.env.PORT || 5000; 
 
 app.use(express.json({ limit: "30mb", extended: true })); 
 app.use(express.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors()); 
 
 //Connect to mongodb cloud atlas 
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+); 
+const connection = mongoose.connection; 
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully"); 
+})
+
+mongoose.set('useFindAndModify', false); 
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`); 
+});
