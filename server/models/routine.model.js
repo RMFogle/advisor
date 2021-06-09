bc9d7a0fbd0a90a3c8f9cb9c48206679354fbf86
RyFogle <13fogle@gmail.com>
2021-06-08 21:13:30 -0700

    adding schemas

---
+++ server/models/routine.model.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'; 
+
+const routineSchema = mongoose.Schema({
+    title: String, 
+}, {
+    timestamps: true, 
+}); 
+
+const RoutineSchema = mongoose.model('RoutineSchema', routineSchema); 
+
+export default RoutineSchema; 
