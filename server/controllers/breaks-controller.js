dad739fa7a737c449d59b398b20e8455545ef4c2
RyFogle <13fogle@gmail.com>
2021-06-09 22:00:14 -0700

    adding get req to breaks controller

--- server/controllers/breaks-controller.js
+++ server/controllers/breaks-controller.js
@@ -1,4 +1,14 @@
+import BreakSchema from '../models/break.model.js'; 
 
-export const getBreaks = (req, res) => {
-    res.send('This works!'); 
+export const getBreaks = async (req, res) => {
+    try {
+        const getBreaks = await BreakSchema.find(); 
+
+        console.log(getBreaks); 
+
+        res.status(200).json(getBreaks); 
+    } catch (error) {
+        res.status(400).json({ message: error.message }); 
+    }
 }
+
