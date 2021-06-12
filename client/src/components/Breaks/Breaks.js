443db926e7f55401fba446d6532f01c99897bec9
RyFogle <13fogle@gmail.com>
2021-06-11 20:14:15 -0700

    adding components

---
+++ client/src/components/Breaks/Breaks.js
@@ -0,0 +1,9 @@
+import React from 'react'; 
+
+const Breaks = () => {
+    return (
+        <h1>BREAKS</h1>
+    ); 
+}
+
+export default Breaks; 
---
+++ client/src/components/Tasks/Tasks.js
@@ -0,0 +1,9 @@
+import React from 'react'; 
+
+const Tasks = () => {
+    return (
+        <h1>TASKS</h1>
+    ); 
+}
+
+export default Tasks; 
