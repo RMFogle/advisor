ba07ba7272454608a13d17cf9fe39679a1775a09
RyFogle <13fogle@gmail.com>
2021-07-22 15:42:06 -0700

    added Timer reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from "redux";
 
 import breaks from './breaks';
+import timersReducer from "./timersReducer";
+import selectedTimerReducer from "./selectedTimeReducer";
 
-export const reducers = combineReducers({ breaks });
+export const reducers = combineReducers({ breaks, timersReducer, selectedTimerReducer });
