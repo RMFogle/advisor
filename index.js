4aa6a7f2603e5e7e203bb5d29571d8682dde9740
RyFogle <13fogle@gmail.com>
2021-10-01 19:49:56 -0700

    fix deploy

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ import dotenv from 'dotenv';
 import agendaRoutes from './server/routes/agenda.js'; 
 import todoRoutes from './server/routes/todos.js';
 
-dotenv.config(); 
 
 const app = express();
-const port = process.env.PORT || 5000; 
+dotenv.config(); 
 
 app.use(express.json({ limit: "30mb", extended: true })); 
 app.use(express.urlencoded({ limit: "30mb", extended: true }))
@@ -21,6 +20,8 @@ app.use('/todos', todoRoutes);
 app.get('/', (req, res) => {
     res.send('Hello to Advisor API')
 });
+
+const port = process.env.PORT || 5000; 
 
 //Connect to mongodb cloud atlas 
 const uri = process.env.ATLAS_URI;
